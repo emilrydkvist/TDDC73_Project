e05ca0c44d02eb92e2ca68783e95255dd62f148b
emilrydkvist <emiry224@student.liu.se>
2015-01-18 14:06:11 +0100

    Password strength meter

---
+++ TDDC73_Project/app/src/main/java/PasswordStrengthMeter.java
@@ -0,0 +1,11 @@
+import android.content.Context;
+import android.widget.EditText;
+
+/**
+ * Created by Emil on 2015-01-16.
+ */
+public class PasswordStrengthMeter extends EditText {
+    public PasswordStrengthMeter(Context context) {
+        super(context);
+    }
+}
